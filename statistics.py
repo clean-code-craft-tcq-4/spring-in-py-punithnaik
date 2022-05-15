5aeb880ae5fab72f9f97c63ece95dd959b95cb57
Naik Punith <pnu5kor@bosch.com>
2022-05-15 11:46:27 +0530

    Add min_max_avg

--- statistics.py
+++ statistics.py
@@ -1,3 +1,6 @@
-
 def calculateStats(numbers):
-  return None
+  avg_num = sum(numbers)/len(numbers)
+  max_num = max(numbers)
+  min_num = min(numbers)
+  d = {"avg":avg_num,"max":max_num,"min":min_num}
+  return d
